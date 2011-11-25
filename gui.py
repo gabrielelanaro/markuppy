9567b452ce1bf72ad948341fef4eb6eeec2ae63a
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2011-11-25 16:34:48 +0100

    Papercuts, menu entries and adding a minumum size requirement

--- gui.py
+++ gui.py
@@ -9,12 +9,14 @@ class MyFrame(wx.Frame):
     def __init__(self, parent=None, id=-1, title="MarKuppY",
                  size=(500,500)):
         super(MyFrame, self).__init__(parent, id, title, size=size)
-        
+
         # this is just to keep things cleaner
         self._create_menu()
         self._create_body()
         self._create_statusbar()
         
+        self.SetMinSize(size) # To prevent collapsing the window when
+                              # resizing
         self.Show()
 
     def _create_menu(self):
@@ -26,17 +28,26 @@ class MyFrame(wx.Frame):
         menubar = wx.MenuBar()
         
         filemenu = wx.Menu()
-        # _b is to specify that it is a "button"
-        quit_b = filemenu.Append(wx.ID_EXIT, '&Quit', "Quit application")
+        # _me is to specify that it is a "menu entry"
+        open_me = filemenu.Append(wx.ID_OPEN)
+        save_in_me = filemenu.Append(wx.ID_ANY, "Save In")
+        save_out_me = filemenu.Append(wx.ID_ANY, "Save Out")
+        quit_me = filemenu.Append(wx.ID_EXIT)
+        
         
         editmenu = wx.Menu()
-        cut_b = editmenu.Append(wx.ID_CUT, 'Cut')
-        copy_b = editmenu.Append(wx.ID_COPY, 'Copy')
-        paste_b = editmenu.Append(wx.ID_PASTE, 'Paste')
+        cut_me = editmenu.Append(wx.ID_CUT)
+        copy_me = editmenu.Append(wx.ID_COPY)
+        paste_me = editmenu.Append(wx.ID_PASTE)
+
+        helpmenu = wx.Menu()
+        manual_me = helpmenu.Append(wx.ID_HELP, 'Manual')
+        about_me = helpmenu.Append(wx.ID_ABOUT)
         
         menubar.Append(filemenu, "&File")
         menubar.Append(editmenu, "Edit")
-
+        menubar.Append(helpmenu, "Help")
+        
         self.SetMenuBar(menubar)
 
         
@@ -62,7 +73,7 @@ class MyFrame(wx.Frame):
         
         # Adding the control widgets to the layout widgets
         convert_controls_box.Add(convert_button, proportion=2,
-                                 flag=wx.EXPAND)
+                                 flag=wx.EXPAND|wx.FIXED_MINSIZE)
         convert_controls_box.Add(format_combo, proportion=1,
                                  flag=wx.EXPAND)
         
